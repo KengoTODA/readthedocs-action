8304d08ea9e3b8d1ffad2126e2284feacc34659e
Kengo TODA <skypencil@gmail.com>
2018-09-03 16:28:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# A GitHub Probot for Read The Docs (RTD) users
+
+## Installation
+
+1. invite `rtd-bot` user to your RTD project.
+2. add `rtd.project` config to `.github/config.yml` file in your repo.
+3. enable rtd-bot in your repo from the rtd-bot page (unpublished).
+
+```yml
+rtd:
+  project: your-read-the-docs-project
+```
+
+## Deployment
---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const puppeteer = require('puppeteer');
+
+module.exports = app => {
+  app.on('pull_request', async context => {
+    // TODO Check if head repo is same with base repo
+
+    // Check if /docs is updated
+    // https://octokit.github.io/rest.js/#api-PullRequests-getFiles
+    const params = context.issue({});
+    const files = context.github.pullRequests.getFiles(params);
+    if (undefined === files.find(file => file.filename.startsWith('docs/'))) {
+      // no need to build RTD document
+      return;
+    }
+
+    // enable RTD build on the target branch
+    const config = await context.config('rtd.yml');
+    const pullRequest = await context.github.pullRequests.get(params);
+    const branch = pullRequest.head.label;
+    enableBuild(config.rtd.project, pullRequest.head.label);
+
+    // report build result with its URL
+    // TODO support other language
+    const url = `https://${config.project}.readthedocs.io/en/${branch}/`;
+    context.github.pullRequests.createComment(context.issue({
+      body: `Check generated document at ${url}`
+    }));
+  })
+}
+
+const enableBuild = async (project, branch) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://readthedocs.org/accounts/login/');
+  await page.type('#id_login', process.env.RTD_USERNAME);
+  await page.type('#id_password', process.env.RTD_PASSWORD);
+  await page.click("button[type='submit']");
+  await page.goto(`https://readthedocs.org/dashboard/${project}/version/${branch}/`); // TODO escape?
+  const checkbox = await page.$(`input#id_active`);
+  const checked = await (await checkbox.getProperty('checked')).jsonValue()
+  if (!checked) {
+    await page.click('input#id_active');
+    await page.click("form[action='.'] input[type=submit]");
+    // TODO consider to set privacy level as 'public'
+    console.info(`enabled RTD build for the branch ${branch}.`);
+  } else {
+    console.info(`RTD build for the branch ${branch} is already active.`);
+  }
+  await browser.close();
+};
