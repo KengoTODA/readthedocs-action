2a811cef1359af35e4da5430caddaec64e7ad760
Kengo TODA <skypencil@gmail.com>
2018-09-04 11:23:29 +0800

    fix: use async function to call another async funcs with await

--- index.js
+++ index.js
@@ -43,35 +43,37 @@ const enableBuild = async (project, branch) => {
   });
 
   return new Promise(function(resolve, reject) {
-    try {
-      const page = await browser.newPage();
-      await page.goto('https://readthedocs.org/accounts/login/');
-      await page.type('#id_login', process.env.RTD_USERNAME);
-      await page.type('#id_password', process.env.RTD_PASSWORD);
-      await page.click("button[type='submit']");
-      await page.screenshot({
-        path: 'login-page.png'
-      });
-      await page.goto(`https://readthedocs.org/dashboard/${project}/version/${branch}/`); // TODO escape?
-      const checkbox = await page.$('input#id_active');
-      await page.screenshot({
-        path: 'version-page.png'
-      });
-      const checked = await (await checkbox.getProperty('checked')).jsonValue()
-      if (!checked) {
-        await page.click('input#id_active');
-        await page.click("form[action='.'] input[type=submit]");
-        // TODO consider to set privacy level as 'public'
-        console.info(`enabled RTD build for the branch ${branch} in ${project}.`);
-        resolve(true);
-      } else {
-        console.debug(`RTD build for the branch ${branch} is already active.`);
-        resolve(false);
+    (async function() {
+      try {
+        const page = await browser.newPage();
+        await page.goto('https://readthedocs.org/accounts/login/');
+        await page.type('#id_login', process.env.RTD_USERNAME);
+        await page.type('#id_password', process.env.RTD_PASSWORD);
+        await page.click("button[type='submit']");
+        await page.screenshot({
+          path: 'login-page.png'
+        });
+        await page.goto(`https://readthedocs.org/dashboard/${project}/version/${branch}/`); // TODO escape?
+        const checkbox = await page.$('input#id_active');
+        await page.screenshot({
+          path: 'version-page.png'
+        });
+        const checked = await (await checkbox.getProperty('checked')).jsonValue()
+        if (!checked) {
+          await page.click('input#id_active');
+          await page.click("form[action='.'] input[type=submit]");
+          // TODO consider to set privacy level as 'public'
+          console.info(`enabled RTD build for the branch ${branch} in ${project}.`);
+          resolve(true);
+        } else {
+          console.debug(`RTD build for the branch ${branch} is already active.`);
+          resolve(false);
+        }
+      } catch (e) {
+        reject(e);
+      } finally {
+        await browser.close();
       }
-    } catch (e) {
-      reject(e);
-    } finally {
-      await browser.close();
-    }
+    })();
   });
 };
