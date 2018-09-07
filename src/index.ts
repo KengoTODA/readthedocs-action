1b29bec6764338c14abf90023d0c719c50285ad5
Kengo TODA <skypencil@gmail.com>
2018-09-07 13:22:15 +0800

    refactor: replace JavaScript with TypeScript

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { Application, Context } from "probot";
+import RTD from "./rtd";
+
+module.exports = (app: Application) => {
+  app.on("pull_request", async (context: Context) => {
+    const log = context.log.child({
+      name: "rtd-bot",
+    });
+    const rtd = new RTD(log);
+
+    // enable RTD build on the target branch
+    const config = await context.config("config.yml", {
+      rtd: {
+        language: "en",
+        project: undefined,
+      },
+    });
+    if (!config || !config.rtd || !config.rtd.project) {
+      context.github.issues.createComment(context.issue({
+        body: "rtd-bot is activated, but .github/config.yml does not have necessary configuration.",
+      }));
+      return;
+    }
+    const project = config.rtd.project + "";
+
+    // Check if head repo is same with base repo
+    if (context.payload.pull_request.base.repo.full_name !== context.payload.pull_request.head.repo.full_name) {
+      log.debug("PR made from another Git repo is not supported.");
+      return;
+    }
+
+    // Check if /docs is updated
+    // https://octokit.github.io/rest.js/#api-PullRequests-getFiles
+    const files = await context.github.pullRequests.getFiles(context.issue({}));
+    if (undefined === files.data.find((file: {filename: string}) => file.filename.startsWith("docs/"))) {
+      log.debug("no need to build RTD document.");
+      return;
+    }
+
+    const branch = context.payload.pull_request.head.ref;
+    const enabled = await rtd.enableBuild(project, branch);
+
+    if (enabled) {
+      // report build result with its URL
+      const url = `https://${project}.readthedocs.io/${config.rtd.language}/${branch}/`;
+      context.github.issues.createComment(context.issue({
+        body: `RTD build has been started. Check generated document at ${url} later`,
+      }));
+    }
+  });
+};
