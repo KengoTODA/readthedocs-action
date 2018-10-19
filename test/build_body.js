a8b8e09704fb976d7ba545db2b277b95aab602ee
Kengo TODA <skypencil@gmail.com>
2018-10-19 11:51:36 +0800

    fix: Stop adding document URL multiple times
    instead of HTML comment, use Markdown comment described at
    https://stackoverflow.com/q/4823468/814928

--- test/build_body.js
+++ test/build_body.js
@@ -4,15 +4,15 @@ const buildBody = require('../lib/build_body.js').default;
 describe('#buildBody()', () => {
   it('adds single URL for project without treanslation', () => {
     const result = buildBody("body", "project", "branch", ["en"]);
-    assert.equal(result, "body\n\n<!-- updated by rtd-bot -->\nURL of RTD document: https://project.readthedocs.io/en/branch/\n");
+    assert.equal(result, "body\n\n[//]: # (rtdbot-start)\n\nURL of RTD document: https://project.readthedocs.io/en/branch/\n\n[//]: # (rtdbot-end)\n");
   });
   it('adds multiple URLs for project with treanslation', () => {
     const result = buildBody("body", "project", "branch", ["en", "ja"]);
-    assert.equal(result, "body\n\n<!-- updated by rtd-bot -->\nURL of RTD documents:\nen: https://project.readthedocs.io/en/branch/\nja: https://project.readthedocs.io/ja/branch/\n");
+    assert.equal(result, "body\n\n[//]: # (rtdbot-start)\n\nURL of RTD documents:\nen: https://project.readthedocs.io/en/branch/\nja: https://project.readthedocs.io/ja/branch/\n\n[//]: # (rtdbot-end)\n");
   });
   it('does not add URL if given body already contains it', () => {
     let result = buildBody("body", "project", "branch", ["en"]);
     result = buildBody(result, "project", "branch", ["en"]);
-    assert.equal(result, "body\n\n<!-- updated by rtd-bot -->\nURL of RTD document: https://project.readthedocs.io/en/branch/\n");
+    assert.equal(result, "body\n\n[//]: # (rtdbot-start)\n\nURL of RTD document: https://project.readthedocs.io/en/branch/\n\n[//]: # (rtdbot-end)\n");
   });
 });
