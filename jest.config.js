b8385c3c4bd6d6dd252215fbb2e88fa68a84d7ef
Kengo TODA <skypencil+github@gmail.com>
2021-06-23 08:56:16 +0800

    test: bump up Jest to v27
    https://jestjs.io/blog/2021/05/25/jest-27

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ["js", "ts"],
-  testEnvironment: "node",
   testMatch: ["**/test/**/*.test.ts", "**/test/**/*.js"],
-  testRunner: "jest-circus/runner",
   transform: {
     "^.+\\.ts$": "ts-jest",
   },
