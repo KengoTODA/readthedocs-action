4c81932de54de49ceec3114944e9d3af28c90fef
Kengo TODA <skypencil@gmail.com>
2022-05-07 14:26:30 +0800

    test: replace test written in JS with TS

--- test/build_body.test.ts
+++ test/build_body.test.ts
@@ -1,6 +1,6 @@
-const assert = require("assert");
-const { Project } = require("../src/rtd");
-const buildBody = require("../src/build_body").default;
+import { describe, expect, it } from "@jest/globals";
+import { Project } from "../src/rtd";
+import buildBody from "../src/build_body";
 
 describe("#buildBody()", () => {
   it("adds single URL for project without treanslation", () => {
@@ -9,8 +9,7 @@ describe("#buildBody()", () => {
       [new Project(1, "en", "project")],
       "branch"
     );
-    assert.equal(
-      result,
+    expect(result).toBe(
       "body\n\n[//]: # (rtdbot-start)\n\n" +
         "URL of RTD document: https://project.readthedocs.io/en/branch/ ![Documentation Status](https://readthedocs.org/projects/project/badge/?version=branch)\n\n" +
         "[//]: # (rtdbot-end)\n"
@@ -22,16 +21,14 @@ describe("#buildBody()", () => {
       [new Project(1, "en", "project"), new Project(2, "ja", "project")],
       "branch"
     );
-    assert.equal(
-      result,
+    expect(result).toBe(
       "body\n\n[//]: # (rtdbot-start)\n\nURL of RTD documents:\nen: https://project.readthedocs.io/en/branch/\nja: https://project.readthedocs.io/ja/branch/\n\n[//]: # (rtdbot-end)\n"
     );
   });
   it("does not add URL if given body already contains it", () => {
     const prev = buildBody("body", [new Project(1, "en", "project")], "branch");
     const result = buildBody(prev, [new Project(1, "en", "project")], "branch");
-    assert.equal(
-      result,
+    expect(result).toBe(
       "body\n\n[//]: # (rtdbot-start)\n\n" +
         "URL of RTD document: https://project.readthedocs.io/en/branch/ ![Documentation Status](https://readthedocs.org/projects/project/badge/?version=branch)\n\n" +
         "[//]: # (rtdbot-end)\n"
