78267c21ae778425cd505563dc70dad0c39a4a6e
Kengo TODA <skypencil@gmail.com>
2022-05-07 14:32:07 +0800

    test: replace test written in JS with TS

--- test/rtd.test.ts
+++ test/rtd.test.ts
@@ -1,7 +1,7 @@
-require("dotenv").config();
+import { describe, expect, it, jest } from "@jest/globals";
+import RTD, { Project } from "../src/rtd";
 
-const assert = require("assert");
-const RTD = require("../src/rtd").default;
+require("dotenv").config();
 
 const timeout = 15 * 1000;
 const integrationTest = process.env["RTD_TOKEN"] ? describe : describe.skip;
@@ -15,7 +15,7 @@ integrationTest("rtd", function () {
       return rtd
         .enableBuild("your-read-the-docs-project", "master")
         .then((enabled) => {
-          assert.ok(!enabled);
+          expect(enabled).toBeFalsy();
         });
     });
   });
@@ -26,7 +26,7 @@ integrationTest("rtd", function () {
       return rtd
         .disableBuild("your-read-the-docs-project", "v0.1.0")
         .then((enabled) => {
-          assert.ok(!enabled);
+          expect(enabled).toBeFalsy();
         });
     });
   });
@@ -35,14 +35,14 @@ integrationTest("rtd", function () {
     it("returns correct ID", async () => {
       const rtd = new RTD(process.env.RTD_TOKEN);
       const result = await rtd.getProject("your-read-the-docs-project");
-      assert.equal(result.id, 235403);
-      assert.equal(result.language, "en");
+      expect(result.id).toBe(235403);
+      expect(result.language).toBe("en");
     });
     it("returns rejected promise for not existing project", async () => {
       const rtd = new RTD(process.env.RTD_TOKEN);
       return rtd.getProject("not-existing").then(
         () => Promise.reject(new Error("Expected method to reject.")),
-        (err) => assert(err instanceof Error)
+        (err) => expect(err).toBeInstanceOf(Error)
       );
     });
   });
@@ -50,37 +50,21 @@ integrationTest("rtd", function () {
   describe("#getTranslates()", () => {
     it("returns single translate", async () => {
       const rtd = new RTD(process.env.RTD_TOKEN);
-      const result = await rtd.getTranslates({
-        id: 235403,
-        language: "en",
-        slug: "your-read-the-docs-project",
-      });
-      assert.deepEqual(result, [
-        {
-          id: 235403,
-          language: "en",
-          slug: "your-read-the-docs-project",
-        },
+      const result = await rtd.getTranslates(
+        new Project(235403, "en", "your-read-the-docs-project")
+      );
+      expect(result).toStrictEqual([
+        new Project(235403, "en", "your-read-the-docs-project"),
       ]);
     });
     it("returns multiple translates", async () => {
       const rtd = new RTD(process.env.RTD_TOKEN);
-      const result = await rtd.getTranslates({
-        id: 79934,
-        language: "en",
-        slug: "spotbugs-in-kengo-toda",
-      });
-      assert.deepEqual(result, [
-        {
-          id: 79934,
-          language: "en",
-          slug: "spotbugs-in-kengo-toda",
-        },
-        {
-          id: 79941,
-          language: "ja",
-          slug: "spotbugs-in-kengo-toda-ja",
-        },
+      const result = await rtd.getTranslates(
+        new Project(79934, "en", "spotbugs-in-kengo-toda")
+      );
+      expect(result).toStrictEqual([
+        new Project(79934, "en", "spotbugs-in-kengo-toda"),
+        new Project(79941, "ja", "spotbugs-in-kengo-toda-ja"),
       ]);
     });
   });
