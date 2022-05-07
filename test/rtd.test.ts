9f975ead5b41ab81e00d4dcb774672dc5bc65a78
Kengo TODA <skypencil@gmail.com>
2022-05-07 14:47:11 +0800

    test: make sure the token exists before running integration tests

--- test/rtd.test.ts
+++ test/rtd.test.ts
@@ -7,11 +7,12 @@ const timeout = 15 * 1000;
 const integrationTest = process.env["RTD_TOKEN"] ? describe : describe.skip;
 
 integrationTest("rtd", function () {
+  const token = process.env.RTD_TOKEN as string;
   jest.setTimeout(timeout);
 
   describe("#enableBuild()", () => {
     it("should return false if branch is already activated", async () => {
-      const rtd = new RTD(process.env.RTD_TOKEN);
+      const rtd = new RTD(token);
       return rtd
         .enableBuild("your-read-the-docs-project", "master")
         .then((enabled) => {
@@ -22,7 +23,7 @@ integrationTest("rtd", function () {
 
   describe("#disabledBuild()", () => {
     it("should return false if branch is already disabled", async () => {
-      const rtd = new RTD(process.env.RTD_TOKEN);
+      const rtd = new RTD(token);
       return rtd
         .disableBuild("your-read-the-docs-project", "v0.1.0")
         .then((enabled) => {
@@ -33,13 +34,13 @@ integrationTest("rtd", function () {
 
   describe("#getProject()", () => {
     it("returns correct ID", async () => {
-      const rtd = new RTD(process.env.RTD_TOKEN);
+      const rtd = new RTD(token);
       const result = await rtd.getProject("your-read-the-docs-project");
       expect(result.id).toBe(235403);
       expect(result.language).toBe("en");
     });
     it("returns rejected promise for not existing project", async () => {
-      const rtd = new RTD(process.env.RTD_TOKEN);
+      const rtd = new RTD(token);
       return rtd.getProject("not-existing").then(
         () => Promise.reject(new Error("Expected method to reject.")),
         (err) => expect(err).toBeInstanceOf(Error)
@@ -49,7 +50,7 @@ integrationTest("rtd", function () {
 
   describe("#getTranslates()", () => {
     it("returns single translate", async () => {
-      const rtd = new RTD(process.env.RTD_TOKEN);
+      const rtd = new RTD(token);
       const result = await rtd.getTranslates(
         new Project(235403, "en", "your-read-the-docs-project")
       );
@@ -58,7 +59,7 @@ integrationTest("rtd", function () {
       ]);
     });
     it("returns multiple translates", async () => {
-      const rtd = new RTD(process.env.RTD_TOKEN);
+      const rtd = new RTD(token);
       const result = await rtd.getTranslates(
         new Project(79934, "en", "spotbugs-in-kengo-toda")
       );
